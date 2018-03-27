8ab3bc644ec83a5735814649802da81df7f4f319
Radu Oprea <raduoprea34@gmail.com>
2018-03-27 10:30:17 +0300

    Initial Commit

---
+++ StockExchange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StockExchange
+//
+//  Created by Radu Oprea on 3/27/18.
+//  Copyright © 2018 Radu Oprea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
